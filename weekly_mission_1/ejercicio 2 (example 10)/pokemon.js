cd0ad0bb24f4cad6b391123d9793c13a9ead018b
RicardLB <99086968+RicardLB@users.noreply.github.com>
2022-04-15 16:51:54 -0600

    Add files via upload

---
+++ weekly_mission_1/ejercicio 2 (example 10)/pokemon.js
@@ -0,0 +1,8 @@
+export default  class pokemon {
+    constructor (name) {
+      this.name = name
+    }
+    sayHello (message) {
+        console.log(`Mi pokemon ${this.name} te saluda`)
+      }
+  }
